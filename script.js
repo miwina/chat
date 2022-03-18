c528107260470cf87d9be08e1e159f9e493bd500
mariva-lab <80712112+mariva-lab@users.noreply.github.com>
2022-03-18 11:29:34 +0200

    ieladet

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(1)
+//console.log(2)
+let zina=document.querySelector('.manaZina');
+let zinas=document.querySelector('.chataZinas');
+
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+    zinas.innerHTML=zinas.innerHTML + '<br/>'+zina.value;
+
+}
+
+async function ieladetChataZinas()
+{
+    let datiNoServera=await fetch('chatazinas.txt');
+    let dati =await datiNoServera.text();
+    //console.log(dati);
+    zinas.innerHTML=dati;
+}
